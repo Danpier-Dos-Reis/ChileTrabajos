698036672e097b7bb7dd981fb5cc0bc808b108bf
danpier <danpier.professional@gmail.com>
2024-08-03 22:03:56 -0400

    Version one

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from Modules.Models import JobItem
 from datetime import datetime
 
 currentDate = datetime.now()
-links = ["https://www.chiletrabajos.cl/encuentra-un-empleo?2=programador+c%23&13=&fecha=&categoria=&8=&14=&inclusion=&f=2"]
+links = ["https://www.chiletrabajos.cl/encuentra-un-empleo?action=search&order_by=&ord=&within=25&2=desarrollador+web&filterSearch=Buscar",#desarrollador web
+         "https://www.chiletrabajos.cl/encuentra-un-empleo?2=analista+programador&13=&fecha=&categoria=&8=&14=&inclusion=&f=2",#analista programador
+         "https://www.chiletrabajos.cl/encuentra-un-empleo?2=desarrollador+full+stack&13=&fecha=&categoria=&8=&14=&inclusion=&f=2", #desarrollador full stack
+         "https://www.chiletrabajos.cl/encuentra-un-empleo?2=desarrollador+front+end&13=&fecha=&categoria=&8=&14=&inclusion=&f=2#google_vignette"#desarrollador front end
+         ]
 
-n = len(links)
-i = n
 _sound = "./Assets/sound.wav"
 fileTXT = "./CurrentJobs.txt"
 
@@ -18,16 +20,20 @@ myDir = os.path.dirname(fileTXT)
 # Crea los directorios si no existen
 if not os.path.exists(myDir): os.makedirs(myDir)
 
-while i > 0:
-    for link in links:
-        doc = Caller.get_dom(link)
-        jobList = DocGear.list_jobs(doc)
-        for job in jobList:
-            if (job.calendar_date == currentDate):
-                #"a" añade texto
-                with open(fileTXT, "a") as txt: txt.write(f"Job \n\tlink => {job.url_link}\n\tFecha => {job.calendar_date}\n\n")
-                playsound(_sound)
-    i = i-1
-    if(i == 0):
-        i = n
+n = len(links)
+i = 0
+while i <= n:
+    link = links[i]
+    doc = Caller.get_dom(link)
+    jobList = DocGear.list_jobs(doc)
+    for job in jobList:
+        #Test
+        #print(f"JobDate=>{jobList[0].calendar_date.date()}\nCurrentDate=>{currentDate.date()}")
+        if (job.calendar_date.date() == currentDate.date()):
+            #"a" añade texto
+            with open(fileTXT, "a") as txt: txt.write(f"Job \n\tlink => {job.url_link}\n\tFecha => {job.calendar_date}\n\n")
+            playsound(_sound)
+    i = i+1
+    if(i == n):
+        i = 0
         time.sleep(8*60)#8min
